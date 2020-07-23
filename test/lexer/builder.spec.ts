97c87146c83cf057008bd996b5b81809d3fef9ac
jiko21 <daikikojima0531@icloud.com>
2020-07-23 16:50:52 +0900

    Add build rule for code , quoteblock

--- test/lexer/builder.spec.ts
+++ test/lexer/builder.spec.ts
@@ -106,6 +106,41 @@ describe('Lexer.class', () => {
       tag: 'p' as Token,
       content: '画像 <img src="http://i.imgur.com/Jjwsc.jpg" alt="エビフライトライアングル">',
     },
+    {
+      tag: 'blockquote',
+      content: [
+        {
+          tag: 'p',
+          content: 'aaa',
+        },
+        {
+          tag: 'p',
+          content: 'bbb',
+        },
+        {
+          tag: 'blockquote',
+          content: [
+            {
+              tag: 'p',
+              content: 'ccc',
+            },
+            {
+              tag: 'p',
+              content: 'ddd',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      tag: 'h2' as Token,
+      content: 'world',
+    },
+    {
+      tag: 'code',
+      content:
+        '&lt;script src=&quot;hoge.js&quot;&gt;&lt;/script&gt;<br />&lt;script src=&quot;hoge.js&quot;&gt;&lt;/script&gt;',
+    },
   ] as ElementNode[];
   let mdNode: MdNode;
   beforeAll(() => {
@@ -138,7 +173,19 @@ describe('Lexer.class', () => {
   </ol></li>
 </ol>
 <p class="flav-md-text flav-md-p">this is <a href="https://www.google.co.jp/" alt="Google先生">Google先生</a></p>
-<p class="flav-md-text flav-md-p">画像 <img src="http://i.imgur.com/Jjwsc.jpg" alt="エビフライトライアングル"></p>`;
+<p class="flav-md-text flav-md-p">画像 <img src="http://i.imgur.com/Jjwsc.jpg" alt="エビフライトライアングル"></p>
+<blockquote class="flav-md-text flav-md-blockquote">
+  <p class="flav-md-text flav-md-p">aaa</p>
+  <p class="flav-md-text flav-md-p">bbb</p>
+  <blockquote class="flav-md-text flav-md-blockquote">
+    <p class="flav-md-text flav-md-p">ccc</p>
+    <p class="flav-md-text flav-md-p">ddd</p>
+  </blockquote>
+</blockquote>
+<h2 class="flav-md-text flav-md-h2 flav-md-h">world</h2>
+<code class="flav-md-code">
+  &lt;script src=&quot;hoge.js&quot;&gt;&lt;/script&gt;<br />&lt;script src=&quot;hoge.js&quot;&gt;&lt;/script&gt;
+</code>`;
     const actual = mdNode.toHtmlString();
     expect(actual).toEqual(expected);
   });
